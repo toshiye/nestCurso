88fd7af9d90193d616c203b2a8631924b2359e92
toshiye <sasakikenji@gmail.com>
2020-03-21 16:52:48 -0300

    20200321

--- nestjs-task-management/src/auth/user.repository.ts
+++ nestjs-task-management/src/auth/user.repository.ts
@@ -1,6 +1,8 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,7 +11,21 @@ export class UserRepository extends Repository<User> {
 
     const user = new User();
     user.username = username;
-    user.password = password;
-    await user.save();
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+
+    try {
+      await user.save();
+    } catch (e) {
+      if (e.code === '23505') {
+        throw new ConflictException('Username already existts');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
